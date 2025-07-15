2afd4e3583a3b8993a4c52cab9c9fa6a60432baf
LazyAfternoons <lazyafternoons@outlook.it>
2025-07-15 14:45:29 +0200

    chore: align qr code

--- example/src/styles.ts
+++ example/src/styles.ts
@@ -4,6 +4,7 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
     paddingVertical: 16,
     paddingHorizontal: 24,
     gap: 8,
