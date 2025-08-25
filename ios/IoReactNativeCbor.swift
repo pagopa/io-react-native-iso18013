cd7013c2efe17e8b09fa53a4fe06843affe119ef
LazyAfternoons <lazyafternoons@outlook.it>
2025-08-25 09:54:54 +0200

    chore: update documentation

--- ios/IoReactNativeCbor.swift
+++ ios/IoReactNativeCbor.swift
@@ -8,6 +8,8 @@ class IoReactNativeCbor: NSObject {
   
   /**
    Decode base64 or base64url encoded CBOR data to JSON object.
+   Resolves with a string containing the parsed data or rejects with an error code defined in ``ModuleErrorCodes``.
+   This method does not handle nested CBOR data, which will need additional parsing.
    - Parameters:
       - cbor: The base64 or base64url encoded CBOR string.
       - resolve: The promise to be resolved.
@@ -34,6 +36,7 @@ class IoReactNativeCbor: NSObject {
   
   /**
    Decode base64 or base64url encoded mDOC-CBOR data to a JSON object.
+   Resolves with a string containing the parsed data or rejects with an error code defined in ``ModuleErrorCodes``.
    - Parameters:
       - mdoc: The base64 or base64url encoded mDOC-CBOR string.
       - resolve: The promise to be resolved.
@@ -59,6 +62,7 @@ class IoReactNativeCbor: NSObject {
   
   /**
    Decode base64 or base64url encoded issuerSigned attribute part of an mDOC-CBOR.
+   Resolves with a string containing the parsed data or rejects with an error code defined in ``ModuleErrorCodes``.
    - Parameters:
       - issuerSigned: The base64 or base64url encoded mDOC-CBOR containing the issuerSigned data string.
       - resolve: The promise to be resolved.
@@ -84,6 +88,7 @@ class IoReactNativeCbor: NSObject {
   
   /**
    Sign base64 encoded data with COSE and return the COSE-Sign1 object in base64 encoding.
+   Resolves with a string containing the COSE-Sign1 object in base64 encoding or rejects with an error code defined in ``ModuleErrorCodes``.
    - Parameters:
      - payloadData: The base64 or base64url encoded payload to sign.
      - keyTag: The alias of the key to use for signing.
@@ -119,7 +124,8 @@ class IoReactNativeCbor: NSObject {
   }
   
   /**
-   Verifies a COSE-Sign1 object with the provided public key
+   Verifies a COSE-Sign1 object with the provided public key.
+   Resolves with boolean indicating whether or not the verification succeeded or not or rejects with an error code defined in ``ModuleErrorCodes``.
    - Parameters:
       - sign1Data: The COSE-Sign1 object in base64 or base64url encoding.
       - publicKey: The public key in JWK format.
