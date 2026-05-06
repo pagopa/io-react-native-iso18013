efd80ebf2e452c2438b9ef37b0d3811e9d347a01
LazyAfternoons <lazyafternoons@mailbox.org>
2026-05-06 11:43:49 +0200

    chore: update config

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,3 @@
 module.exports = {
-  overrides: [
-    {
-      exclude: /\/node_modules\//,
-      presets: ['module:react-native-builder-bob/babel-preset'],
-    },
-    {
-      include: /\/node_modules\//,
-      presets: ['module:@react-native/babel-preset'],
-      // Must run before @babel/plugin-transform-modules-commonjs to handle
-      // Zod v4's ESM `export * as namespace from '...'` syntax correctly.
-      plugins: ['@babel/plugin-transform-export-namespace-from'],
-    },
-  ],
+  presets: ['module:react-native-builder-bob/babel-preset'],
 };
