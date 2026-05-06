448f80ba84fabaa3e20676b520cb11d7bf2f4ce4
LazyAfternoons <lazyafternoons@mailbox.org>
2026-05-06 11:29:12 +0200

    fix: import error

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  presets: ['module:react-native-builder-bob/babel-preset'],
+  overrides: [
+    {
+      exclude: /\/node_modules\//,
+      presets: ['module:react-native-builder-bob/babel-preset'],
+    },
+    {
+      include: /\/node_modules\//,
+      presets: ['module:@react-native/babel-preset'],
+      // Must run before @babel/plugin-transform-modules-commonjs to handle
+      // Zod v4's ESM `export * as namespace from '...'` syntax correctly.
+      plugins: ['@babel/plugin-transform-export-namespace-from'],
+    },
+  ],
 };
